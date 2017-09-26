cd9cbe24b537d0bf7955a81ff92793500bf319b0
bltnico <bellot.n@gmail.com>
2017-09-26 23:43:22 +0200

    Add Button proptypes

--- src/Button.js
+++ src/Button.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Button.css';
 
 class Button extends Component {
+
+  static propTypes = {
+    label: PropTypes.string.isRequired,
+    style: PropTypes.object,
+    disabled: PropTypes.bool,
+    onPress: PropTypes.func,
+  };
+
+  static defaultProps = {
+    style: {},
+    disabled: false,
+    onPress: () => {},
+  };
+
   render() {
-    const { label, onPress } = this.props;
+    const { label, style, disabled, onPress } = this.props;
     return (
       <button
         className='Button'
         type='button'
+        style={style}
+        disabled={disabled}
         onClick={onPress}>
         {label}
       </button>
