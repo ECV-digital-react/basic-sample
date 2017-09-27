ebd4c7a6d35ab9ccfc3b724e61ad1e6e46d8a137
bltnico <bellot.n@gmail.com>
2017-09-27 17:30:56 +0200

    Button Component to Stateless component

--- src/Button.js
+++ src/Button.js
@@ -2,34 +2,28 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Button.css';
 
-class Button extends Component {
+const Button = ({ label, style, disabled, onPress }) => (
+  <button
+    className='Button'
+    type='button'
+    style={style}
+    disabled={disabled}
+    onClick={onPress}>
+    {label}
+  </button>
+);
 
-  static propTypes = {
-    label: PropTypes.string.isRequired,
-    style: PropTypes.object,
-    disabled: PropTypes.bool,
-    onPress: PropTypes.func,
-  };
+Button.propTypes = {
+  label: PropTypes.string.isRequired,
+  style: PropTypes.object,
+  disabled: PropTypes.bool,
+  onPress: PropTypes.func,
+};
 
-  static defaultProps = {
-    style: {},
-    disabled: false,
-    onPress: () => {},
-  };
-
-  render() {
-    const { label, style, disabled, onPress } = this.props;
-    return (
-      <button
-        className='Button'
-        type='button'
-        style={style}
-        disabled={disabled}
-        onClick={onPress}>
-        {label}
-      </button>
-    );
-  }
-}
+Button.defaultProps = {
+  style: {},
+  disabled: false,
+  onPress: () => {},
+};
 
 export default Button;
